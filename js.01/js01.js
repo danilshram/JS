0fde0318ef575284fba99d42e9f01a4e7fd451aa
danilshram <danilshram29@gmail.com>
2023-03-24 19:18:33 +0100

    js

---
+++ js.01/js01.js
@@ -0,0 +1,7 @@
+let income = prompt('day income');
+let expenses = prompt('day expenses');
+let weekIncome = income * 7;
+let weekExpenses = expenses * 7;
+let result = weekIncome - weekExpenses;
+console.log('result: ' + result);
+
