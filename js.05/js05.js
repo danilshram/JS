87b9d47f932f811fb0c9af6ab45212d6acbbdd63
danilshram <danilshram29@gmail.com>
2023-04-18 23:04:38 +0200

    arrays

--- js.05/js05.js
+++ js.05/js05.js
@@ -104,9 +104,75 @@ console.log(copyMultiplicationTable)
     console.log(multiplicationTable2)
 }
 
-// {
-//     const word = prompt("Введіть слово");
-//     let arr = word.split('')
-//     const [a,,,,b,,,,c] = arr
-//     console.log(arr)
-// } НЕ ДОРОБЛЕНО
+{
+    const word = prompt("Введіть слово");
+    let [a,,,,b,,,,c] = word.split('');
+    console.log([a,b,c])
+} 
+
+
+{
+    const word = prompt("Введіть слово");
+    let [,a=!,,b=!, c=! ] = word.split('');
+    console.log([a,b,c])
+}
+
+
+{
+    const multiplicationTable = [
+        [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        [0, 2, 4, 6, 8, 10, 12, 14, 16, 18],
+        [0, 3, 6, 9, 12, 15, 18, 21, 24, 27],
+        [0, 4, 8, 12, 16, 20, 24, 28, 32, 36],
+        [0, 5, 10, 15, 20, 25, 30, 35, 40, 45],
+        [0, 6, 12, 18, 24, 30, 36, 42, 48, 54],
+        [0, 7, 14, 21, 28, 35, 42, 49, 56, 63],
+        [0, 8, 16, 24, 32, 40, 48, 56, 64, 72],
+        [0, 9, 18, 27, 36, 45, 54, 63, 72, 81]
+        ];
+    let [[a, b, ...rest1], [c, d, ...rest2], [e, f, ...rest3], [g, h, ...rest4]] = multiplicationTable;
+    multiplicationTable2 = [b, ...rest1, d, ...rest2, f, ...rest3, h, ...rest4];
+    console.log(multiplicationTable2)
+
+}
+
+
+{
+    let names = ["John", "Paul", "George", "Ringo"];
+    for(const man of names) {
+        alert(man)
+    }
+}
+
+
+{
+    const currencies = ["USD", "EUR", "GBP", "UAH"]
+    let   str = "<select>"
+    for (const currency of currencies){
+       str += `<option>${currency}</option>`
+    }
+    str+= "</select>"
+    document.write(str)
+
+}
+
+{
+    const names = ["John", "Paul", "George", "Ringo"]
+    let   str = "<table>"
+    for (const name of names){
+        str += name + ` `
+    }
+    str+= "</table>"
+    document.write(str) 
+}
+
+
+{
+    const names = ["John", "Paul", "George", "Ringo"]
+    let   str = "<table>"
+    for (const name of names){
+        str += name + `\n`
+    }
+    str+= "</table>"
+    document.write(str)
+}
