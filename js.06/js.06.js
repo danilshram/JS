9ac7a2e5d1009431da13699e8254f5f38030ac6a
danilshram <danilshram29@gmail.com>
2023-04-23 19:26:18 +0200

    objects

--- js.06/js.06.js
+++ js.06/js.06.js
@@ -34,12 +34,14 @@
     const body = {
         tagName: 'body',
         children: [
-            {
+            { 
                 tagName: 'div',
                 children: [
                     {
                         tagName: 'span',
-                        children: ["Enter a data please"],
+                        children: [`Enter a data please`],
+                    },
+                    {
                         tagName: 'br'
                     },
                     {
@@ -53,17 +55,19 @@
                         tagName: 'input',
                         attrs: {
                             type: 'text',
-                            id: 'name'
+                            id: 'surname'
                         }
                     }
                 ],
+            },
+            {
                 tagName: 'div',
                 children: [
                     {
                         tagName: 'button',
                         children: ['ok'],
                         attrs: {
-                            id: 'ok'
+                            id: 'ok',
                         }
                     },
                     {
@@ -73,9 +77,31 @@
                             id: 'cancel'
                         }
                     }
-                
                 ]
             }
         ]
     }
+    console.log(body.children[1].children[1].children)
+    console.log(body.children[0].children[3].attrs['id'])
+    body.children[1].children[0].attrs[prompt("Введіть атрибут")] = prompt("Введіть значення") 
+    body.tagName = body
+    body.children[0].body = body
+    body.children[1].body = body
+    body.children[0].children[0] = body.children[0]
+    body.children[0].children[1] = body.children[0]
+    body.children[0].children[2] = body.children[0]
+    body.children[0].children[3] = body.children[0]
+    body.children[1].children[0] = body.children[1]
+    body.children[1].children[1] = body.children[1]
 }
+
+
+{
+    let arr = [1,2,3,4,5, "a", "b", "c"]
+    const [,even1,,even2,,] = arr
+    const [odd1,,odd2,,odd3] = arr
+    const [,,,,,...arr2] = arr
+}
+  
+
+
