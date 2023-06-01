d18f27a380e36ab1ef83014bf8bae7195b3d88e9
danilshram <danilshram29@gmail.com>
2023-06-01 13:12:57 +0200

    fooop

--- js.13/js.13.js
+++ js.13/js.13.js
@@ -43,7 +43,7 @@ function Password(parent, open){
             checkbox.innerText = 'Show'
         }
     }
-    this.onChange = function(){
+    this.onChange = () => {
         return passwordInput.value
     }
     this.onOpenChange = function(){
@@ -102,7 +102,7 @@ function Password(parent, open){
         return this.status
     }
     passwordInput.oninput = () => {
-         this.onChange(passwordInput.value)
+        this.onChange(passwordInput.value)
     }
     checkbox.onclick = () =>{
         this.setOpen(!this.status)
@@ -129,61 +129,151 @@ loginInput.oninput = p.onChange = () => {
 }
 
 
+function LoginPassword(parent, open){
+    let loginInput = document.createElement('input')
+    let passwordInput = document.createElement('input')
+    let checkButton = document.createElement('button')
+    checkButton.innerText = 'GO'
+    checkButton.disabled = true
+    loginInput.type = 'text'
+    this.status = open
+    parent.append(loginInput)
+    parent.append(passwordInput)
+    parent.append(checkButton)
+
+    this.getLoginValue = function(){
+        return loginInput.value
+    }
+    this.getPasswordValue = function(){
+        return passwordInput.value
+    }
+    this.setLoginValue = function(newValue){
+        loginInput.value = newValue 
+        return loginInput.value
+    }
+    this.setPasswordValue = function(newValue){
+        return passwordInput.value = newValue
+    }
+    this.getCheckButton = function(){
+        return this.status
+    }
+    this.setCheckButton = function(status){   
+        return this.status = status  
+    }
+    this.onChange = function(){         
+        return loginInput.value
+    }
+    this.onChange2 = function(){
+        return passwordInput.value
+    }
+    this.onButtonChange = function(status){      
+        return status 
+    }
+    loginInput.oninput = () =>{
+       this.onChange(loginInput.value)
+     }
+     passwordInput.oninput = () => {
+        this.onChange2(passwordInput.value)    
+        if(loginInput.value !== "" && passwordInput.value !==""){
+            checkButton.disabled = false
+         }else{
+             checkButton.disabled = true
+         }
+     }
+    checkButton.onclick = () =>{
+        this.setCheckButton(!this.status)
+        this.onButtonChange(this.status)
+    }
+    this.setCheckButton(open)
+    }
+const b = new LoginPassword(document.body, true)
+b.setLoginValue('')
+console.log(b.getLoginValue())    
 
 
-// function Login(parent, open){
-//     let loginInput = document.createElement('input')
-//     let checkButton = document.createElement('button')
-//     checkButton.innerText = 'GO'
-//     loginInput.type = 'text'
-//     this.status = open
-//     parent.append(loginInput)
-//     parent.append(checkButton)
 
-//     this.getLoginValue = function(){
-//         return loginInput.value
-//     }
-//     this.setLoginValue = function(newValue){
-//         loginInput.value = newValue 
 
-//         return loginInput.value
-//     }
-//     this.getCheckButton = function(){
-//         return this.status
-//     }
-//     this.setCheckButton = function(status){   
-//         this.status = status  
-//         if(this.status && loginInput.value.length > 0){
-//             checkButton.disabled = false
-//         }else{
-//             checkButton.disabled = true
-//         }    
-//     }
-//     this.onChange = function(){       
-//         return loginInput.value
-//     }
-//     this.onButtonChange = function(status){      
-//         return status
-//     }
-//     loginInput.oninput = () => this.onChange(loginInput.value)
-//     checkButton.onclick = () =>{
-//         this.setCheckButton(!this.status)
-//         this.onButtonChange(this.status)
-//     }
-//     this.setCheckButton(open)
-// }
-// const b = new Login(document.body, true)
-// b.setLoginValue('')
-// console.log(b.getLoginValue())    
-// // passwordInput.oninput = this.onChange(passwordInput.value) = () => {
-//         if(loginInput.value === "" && passwordInput.value === ""){
-//             checkbox.disabled = true
-//         }else{
-//             checkbox.disabled = false
-//         }  
-        
-//     // }
-//     // checkbox.onclick = () =>{
-//     //     this.setOpen(!this.status)
-//     //     this.onOpenChange(this.status)
-//     // }
+
+function PasswordVerify(parent){
+    let passwordInput = document.createElement('input')
+    let checkbox = document.createElement('button') 
+    checkbox.innerText = open ? 'Hide' : 'Show'
+    let checkPasswordInput = document.createElement('input')
+    checkPasswordInput.type = 'password'
+    checkPasswordInput.style.display = 'initial'
+
+    this.status = false
+    parent.append(passwordInput)
+    parent.append(checkPasswordInput)
+    parent.append(checkbox)
+
+    this.getValue = function(){
+        return passwordInput.value
+    }
+    this.getCheckValue = function(){
+        return checkPasswordInput.value
+    }
+    this.setValue = function(newValue) {
+        passwordInput.value = newValue
+        return passwordInput.value
+    }    
+    this.setCheckValue = function(newValue){
+        return checkPasswordInput.value = newValue
+    }
+    this.getOpen = function(){
+        return this.status
+    } 
+    this.setOpen = function(status) {
+        this.status = status
+        if(this.status){
+            passwordInput.type = 'text'
+            checkbox.innerText = 'Hide'
+        }else{
+            passwordInput.type = 'password'
+            checkbox.innerText = 'Show'
+        }
+    }
+    this.setStyle = (style,style2) => {
+        passwordInput.style.borderColor = style
+        checkPasswordInput.style.borderColor = style2
+    }
+    this.onChange = () => {
+        passwordInput.value
+    }
+    this.onChange2 = () =>{
+        checkPasswordInput.value
+    }
+    this.onOpenChange = function(){
+        return this.status
+    }
+    checkPasswordInput.oninput = () =>{
+        this.onChange2(checkPasswordInput.value)
+        if(checkPasswordInput.value !== passwordInput.value){
+            this.setStyle('red', 'red')
+        }else{
+            this.setStyle('black','black')
+        }  
+    }
+    passwordInput.oninput = () => {
+        this.onChange(passwordInput.value)
+        if(passwordInput.value !== checkPasswordInput.value){
+        this.setStyle('red', 'red')
+        }else{
+        this.setStyle('black','black')
+        }   
+    }
+    checkbox.onclick = () =>{        
+        if(!this.status){
+            checkPasswordInput.style.display = 'none'
+        }else{
+            checkPasswordInput.style.display = 'initial'
+        }    
+        this.setOpen(!this.status)
+        this.onOpenChange(this.status)
+
+    }
+    this.setOpen(this.status)
+}
+let newDiv = document.createElement('div')
+document.body.append(newDiv)
+const password = new PasswordVerify(newDiv,true)
